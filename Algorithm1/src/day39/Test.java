511f565271e09859078055ad0be8ea3e2ec80a22
qazq1212612 <361864254@qq.com>
2017-10-29 20:19:06 +0800

    day39

--- Algorithm1/src/day39/Test.java
+++ Algorithm1/src/day39/Test.java
@@ -1,18 +1,94 @@
 package day39;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Test aTest=new Test();
-		System.out.println(~4);
-		System.out.println(((int)Math.abs(~4)));
-		System.out.println(aTest.integerBreak(10));
+		System.out.println(aTest.reverseVowels("leetcode"));
 	}
+	
+	public List<Integer> topKFrequent(int[] nums, int k) {
+		if(nums.length<1)
+			return new ArrayList<>();
+		List<Integer>[] bucket=new List[nums.length+1];
+		Map<Integer, Integer> map=new HashMap<>();
+		for(int num:nums)
+		{
+			if(map.containsKey(num))
+			{
+				map.put(num, map.get(num)+1);
+			}
+			else
+			{
+				map.put(num, 1);
+			}
+		}
+		for (int key : map.keySet()) {
+			int frequency = map.get(key);
+			if (bucket[frequency] == null) {
+				bucket[frequency] = new ArrayList<>();
+			}
+			bucket[frequency].add(key);
+		}
+
+		List<Integer> res = new ArrayList<>();
+
+		for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+			if (bucket[pos] != null) {
+				res.addAll(bucket[pos]);
+			}
+		}
+		return res;
+		
+	}
+    public String reverseVowels(String s) {
+    	if(s.length()<2)
+    		return s;
+    	char[] res=s.toCharArray();
+    	int left=0, right=s.length()-1;
+    	Set<Character> set=new HashSet<>();
+    	set.add('a');
+    	set.add('e');
+    	set.add('i');
+    	set.add('o');
+    	set.add('u');
+    	set.add('A');
+    	set.add('E');
+    	set.add('I');
+    	set.add('O');
+    	set.add('U');
+    	while(left<right)
+    	{
+    		while(left<right&&!set.contains(res[left]))
+    			left++;
+    		while(right>left&&!set.contains(res[right]))
+    			right--;
+    		if(left>=right)
+    			return String.valueOf(res);
+    		else
+    		{
+    			char temp=res[left];
+    			res[left]=res[right];
+    			res[right]=temp;
+    			left++;
+    			right--;
+    		}
+    	}
+    	return String.valueOf(res);
+    }
+    public String reverseString(String s) {
+    	StringBuilder sb=new StringBuilder(s);
+    	return sb.reverse().toString();
+    }
     public int integerBreak(int n) {
     	int[] dp= new int[n+1];
     	dp[1]=1;
